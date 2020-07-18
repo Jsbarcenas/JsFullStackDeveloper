d4e2cd9eab36ab8a81ba84d30f39cc1f8bb6d630
jsbarcenasPort <jsbarcenas00@gmail.com>
2020-07-18 13:20:18 -0500

    agregando unas cositas

---
+++ Clase_nÃumeros.js
@@ -0,0 +1,7 @@
+var decimal = 200.3;
+var resultado = decimal * 3;
+
+
+resultado00 = ( resultado *100)/100;//Con esto 100 y 1/100 selecionamos lacatnidad de decimales que queremos ver.}
+var resultadoMath = Math.round(resutlado);// redondea directamente pero no maneja bien los decimales.
+
--- clase2.js
+++ clase2.js
@@ -8,4 +8,5 @@ var cantidadDeLetrasDelNombre = nombre.length
 
 var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`
 
-var str = nombre.substr(1, 2)
+var str = nombre.substr(cantidadDeLetrasDelNombre - 1);
+var mostrarUltimaLetra = nombre.charAt(cantidadDeLetrasDelNombre - 1);
