5efdf94dca828ce287fcfe43932a1b460873f947
jsbarcenasPort <jsbarcenas00@gmail.com>
2020-08-01 15:42:37 -0500

    Actualizar archivos en e l computador

---
+++ Scope-Clousure/src/scope/local.js
@@ -0,0 +1,10 @@
+
+var scope = "i am global";
+const functionScope = () => {
+    var scope = "i am just a local";
+    const func = () => {
+        return scope
+    }
+    console.log(func());
+}
+functionScope();
