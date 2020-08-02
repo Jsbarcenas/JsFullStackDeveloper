7adf4337eeb40390ea7656efa791454a3d266161
Santiago Bárcenas <jsbarcenas00@gmail.com>
2020-08-02 09:26:28 -0500

    Agregando contenido superado

---
+++ Scope-Clousure/src/closure/private.js
@@ -0,0 +1,18 @@
+//Variable privada o propiedades privadas
+
+const person = () => {
+    var saveName = "Name";
+    return {
+        getName: () => {
+            return saveName;
+        },
+        setName: (Name) => {
+            saveName = Name;
+        }
+    }
+}
+
+newPerson = person();
+console.log(newPerson.getName());
+newPerson.setName("Santiago");
+console.log(newPerson.getName());
