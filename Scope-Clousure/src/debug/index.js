7a93047b11c5e463738800c3d9a5ce349c8c2abe
Santiago Bárcenas <jsbarcenas00@gmail.com>
2020-08-02 11:05:57 -0500

    Curso de Scope y closure termiando!

---
+++ Scope-Clousure/src/debug/index.js
@@ -0,0 +1,10 @@
+var a = "Hello";
+function hello () {
+    let b = "Hello world";
+    const c = "Hello Worl!";
+    if (true) {
+        let d = "Helllo World!!!!";
+        debugger;
+    }
+}
+hello();
