6fd3b697da22ed1a09ed32af66f7fe8b4edf97b7
Santiago Bárcenas <jsbarcenas00@gmail.com>
2020-08-05 00:09:33 -0500

    Callback dominados

--- AsincronismoJS/src/callback/challenge.js
+++ AsincronismoJS/src/callback/challenge.js
@@ -1,5 +1,5 @@
-let XMLHttpRequest = requier(`xmlhttprequest`).XMLHttpRequest;
-
+let XMLHttpRequest = require(`xmlhttprequest`).XMLHttpRequest;
+const API = `https://rickandmortyapi.com/api/character/`;
 function fetchData (url_api,callback) {
     let xhttp = new XMLHttpRequest();
     xhttp.open(`GET`,url_api,true);//el tercer parámetro es el asynchronims
@@ -13,5 +13,23 @@ function fetchData (url_api,callback) {
             }
         }
     }
+    xhttp.send();
 }
-xhttp.send()
+fetchData(API, function (error1, data1) {
+    if(error1){
+        return console.log(error1);
+    }
+    fetchData(API + data1.results[0].id, function(error2 , data2) {
+        if(error2){
+            return console.log(error2);
+        }
+        fetchData(data2.origin.url, function (error3, data3){
+            if(error3) {
+                return console.log(error3);
+            }
+            console.log(data1.info.count);
+            console.log(data2.name);
+            console.log(data3.dimension);
+        })
+    } )
+});
