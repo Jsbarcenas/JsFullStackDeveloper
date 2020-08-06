6a851321d14a491a6b55178fe219e12b4ea234cd
Santiago Bárcenas <jsbarcenas00@gmail.com>
2020-08-05 22:16:27 -0500

    haciendo mi propio callback a una Api

--- AsincronismoJS/src/callback/challenge.js
+++ AsincronismoJS/src/callback/challenge.js
@@ -3,10 +3,10 @@ const API = `https://rickandmortyapi.com/api/character/`;
 function fetchData (url_api,callback) {
     let xhttp = new XMLHttpRequest();
     xhttp.open(`GET`,url_api,true);//el tercer parámetro es el asynchronims
-    xhttp.onreadystatechange = function(event) {
+    xhttp.onreadystatechange = function (event) {
         if(xhttp.readyState === 4) {
             if(xhttp.status === 200) {
-                return callback(null, JSON.parse(xhttp.responseText));
+                return callback (null, JSON.parse(xhttp.responseText));
             }else { 
                 const error = new Error(`Error ${url_api}`)
                 return callback (error,null);
@@ -33,3 +33,35 @@ fetchData(API, function (error1, data1) {
         })
     } )
 });
+
+
+let XMLHttpRequest = require(`xmlhttprequest`).XMLHttpRequest;
+const API_URL = "https://swapi.dev/api/people/";
+
+function fetchStarWarsApi (API_URL,callback) {
+    let xhttp = new XMLHttpRequest();
+    xhttp.open(`GET`,API_URL,true);
+    xhttp.onreadystatechange = function (event) {
+        if(xhttp.readyState === 4) {
+            if(xhttp.status === 200) {
+                return callback (null, JSON.parse(xhttp.responseText));
+            }else{
+                console.log(xhttp.status);
+                const error = new Error(`Error en ${API_URL}`);
+                return callback(error, null);
+            }
+        }
+    }
+    xhttp.send();
+}
+
+ fetchStarWarsApi (API_URL, function (error1, data1){
+    if (error1){
+        return console.log(error1);
+    }
+    console.log(data1);
+    console.log(data1.count);
+    console.log(data1.results[0].name);
+    console.log(data1.results[0].homeworld);
+    
+})
