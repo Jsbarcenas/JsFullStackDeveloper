19b117ca5f23d2bab2d71cca6c8363416a2b140d
Santiago Bárcenas <jsbarcenas00@gmail.com>
2020-08-26 19:55:50 -0500

    AsyncAwait

---
+++ CursoNode/src/async/asyncAwait.js
@@ -0,0 +1,45 @@
+const soyAsincrona =  (nombre) => {
+    return new Promise( (resolve, reject) => {
+        console.log("Hola, soy "+nombre+ ", y estoy viviendo de forma asíncrona");
+    setTimeout(() => {
+        var i = 2;
+      console.log("Estoy siendo asincrona gracias a " + "Santiago");
+      resolve(nombre);
+    }, 1000);
+    });
+ 
+  };
+  const adios = (nombre) => {
+      return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            console.log(`Adiós ${nombre}`);
+          resolve();
+          }, 2000);
+      })
+    
+  };
+  
+  const hablar = (nombre) =>{
+    return new Promise((resolve,reject) => {
+        setTimeout(() => {
+            console.log("balbala");
+            resolve(nombre)
+            
+          }, 1000);
+    })
+    
+}
+
+
+async function main () {
+     await soyAsincrona("sanitgo");
+     await hablar();
+     await hablar();
+     await hablar();
+     await hablar();
+     await adios("Santi");
+}
+
+console.log('hola, empezamos');
+main();
+console.log('Hola, terminamos');
